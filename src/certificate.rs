4b159bc484817ac93e17fc95d29a28bfb3f8b769
Raito Bezarius <masterancpp@gmail.com>
2023-06-15 06:21:22 +0200

    help

--- src/certificate.rs
+++ src/certificate.rs
@@ -1,18 +1,58 @@
-use goblin::pe::certificate_table::{AttributeCertificate, AttributeCertificateType};
 use cms::content_info::ContentInfo;
-use x509_cert::der::{Decode, Result};
+use cms::signed_data::SignedData;
+use goblin::pe::certificate_table::{AttributeCertificate, AttributeCertificateType};
+use x509_cert::der::{Encode, Decode, Sequence, AnyRef, Reader};
+use x509_cert::der::asn1::{ObjectIdentifier, OctetStringRef};
+use x509_cert::spki::AlgorithmIdentifier;
+
+/// SPC_INDIRECT_DATA_OBJID http://oid-info.com/get/1.3.6.1.4.1.311.2.1.4
+pub const SPC_INDIRECT_DATA_OBJID: ObjectIdentifier = ObjectIdentifier::new_unwrap("1.3.6.1.4.1.311.2.1.4");
+
+#[derive(Clone, Debug, Eq, PartialEq, Sequence)]
+pub struct DigestInfo<'a> {
+    // Technically: RFC3279 only?
+    // Not updates, or is it?
+    pub digest_algorithm: AlgorithmIdentifier<AnyRef<'a>>,
+    pub digest: OctetStringRef<'a>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Sequence)]
+struct SpcIndirectDataContent<'a> {
+    pub data: ContentInfo,
+    pub message_digest: DigestInfo<'a>
+}
 
 pub trait AttributeCertificateExt {
-    fn as_signed_data(&self) -> Option<Result<ContentInfo>>;
+    fn as_signed_data(&self) -> Option<Result<SignedData>>;
+    fn as_spc_indirect_data_content(&self) -> Option<Result<SpcIndirectDataContent>>;
 }
 
 impl<'a> AttributeCertificateExt for AttributeCertificate<'a> {
     /// Return the pkcs7 [`ContentInfo`] attached to the [`PE`]
-    fn as_signed_data(&self) -> Option<Result<ContentInfo>> {
+    fn as_signed_data(&self) -> Option<Result<SignedData>> {
         if self.certificate_type == AttributeCertificateType::PkcsSignedData {
-            Some(ContentInfo::from_der(self.certificate))
+            Some(
+                ContentInfo::from_der(self.certificate).and_then(|cinfo| cinfo.content.decode_as::<SignedData>())
+            )
         } else {
             None
         }
     }
+
+    fn as_spc_indirect_data_content(&self) -> Option<Result<SpcIndirectDataContent>> {
+        self.as_signed_data()
+            .map(|maybe_sdata| {
+                if let Ok(sdata) = maybe_sdata {
+                    if sdata.encap_content_info.econtent_type != SPC_INDIRECT_DATA_OBJID {
+                        return None;
+                    }
+
+                    // This is bad
+                    Some(sdata.encap_content_info.econtent.unwrap().decode_as::<SpcIndirectDataContent<'a>>())
+                } else {
+                    None
+                }
+            })
+    }
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,31 @@
 //!
 //! Goblin signing is intended to be a support library for goblin to help with the signature of
 //! executable.
+
+use authenticode::Authenticode;
+use certificate::AttributeCertificateExt;
+use digest::Digest;
+use goblin::pe::PE;
+use x509_cert::Certificate;
 pub mod authenticode;
 pub mod certificate;
+
+pub fn sign(pe: PE, certificate: Certificate) -> PE {
+    pe
+}
+
+// Verify that PE's Authenticode
+// match the one provided in the signatures.
+// pub fn verify_authenticode_in_signatures<D: Digest>(pe: PE) -> bool {
+//     let pe_authenticode_digest = pe.authenticode_digest::<D>();
+//     // TODO: filter out correctly the digest by D
+//     pe.certificates
+//         .into_iter()
+//         .all(|cert| {
+//             match cert.as_digest_info() {
+//                 Some(Ok(digest_info)) => digest_info.digest.as_bytes().into_iter().zip(pe_authenticode_digest).all(|(a, b): (&u8, u8)| *a == b),
+//                 Some(_) => false,
+//                 None => false
+//             }
+//         })
+// }
